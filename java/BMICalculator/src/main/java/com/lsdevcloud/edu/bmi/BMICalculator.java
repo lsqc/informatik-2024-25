0e46846af0538742108ba4e821c2aa1e66857449
lsdevcloud <info@lsdevcloud.com>
2025-01-13 16:19:55 +0100

    fixed BMICalculator (replaced Scanner#nextLine with Scanner#next due to issues with name input reading)

--- java/BMICalculator/src/main/java/com/lsdevcloud/edu/bmi/BMICalculator.java
+++ java/BMICalculator/src/main/java/com/lsdevcloud/edu/bmi/BMICalculator.java
@@ -8,35 +8,30 @@ import java.util.Scanner;
  *
  * @author lsdevcloud
  */
-public final class BMICalculator
-{
-    public static void main(String[] args)
-    {
+public final class BMICalculator {
+    public static void main(String[] args) {
 
         final Scanner scanner = new Scanner(System.in);
 
-        while (true)
-        {
+        while (true) {
             // height in centimeters
             // weight in kilograms
             double height = 0, weight = 0;
             String name;
 
-            try
-            {
+            try {
                 System.out.print("Name? \n> ");
-                name = scanner.nextLine().trim();
+                name = scanner.next().trim();
 
                 System.out.print("Gewicht in KG? \n> ");
                 weight = scanner.nextDouble();
 
-                if (weight <= 0) System.exit(0);
+                if (weight <= 0)
+                    System.exit(0);
 
                 System.out.print("Größe im cm? \n> ");
                 height = scanner.nextDouble();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.err.println("Ungültige Zahl für Gewicht/Größe erhalten.");
                 scanner.nextLine();
                 continue;
@@ -49,20 +44,13 @@ public final class BMICalculator
             System.out.println("Errechneter BMI: " + bmi);
 
             System.out.printf("Ergebnis für %s: ", name);
-            if (bmi < 18.5)
-            {
+            if (bmi < 18.5) {
                 System.out.print("Untergewicht");
-            }
-            else if (bmi >= 18.5 && bmi < 25)
-            {
+            } else if (bmi >= 18.5 && bmi < 25) {
                 System.out.print("Normalgewicht");
-            }
-            else if (bmi >= 25 && bmi <= 30)
-            {
+            } else if (bmi >= 25 && bmi <= 30) {
                 System.out.print("Präadipositas");
-            }
-            else
-            {
+            } else {
                 System.out.print("Adipositas");
             }
 
